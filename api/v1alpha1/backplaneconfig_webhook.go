d335d09863c117681a54625a479075e6ab0e8816
Zack Kayyali <zkayyali@redhat.com>
2021-08-31 11:47:56 -0400

    Expand Webhook Logic (#31)
    Signed-off-by: Zachary Kayyali <zkayyali@redhat.com>

--- api/v1alpha1/backplaneconfig_webhook.go
+++ api/v1alpha1/backplaneconfig_webhook.go
@@ -21,10 +21,17 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"strings"
 
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/discovery"
 	ctrl "sigs.k8s.io/controller-runtime"
 	cl "sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 )
@@ -33,6 +40,50 @@ import (
 var (
 	backplaneconfiglog = logf.Log.WithName("backplaneconfig-resource")
 	Client             cl.Client
+
+	blockCreationResources = []struct {
+		Name string
+		GVK  schema.GroupVersionKind
+	}{
+		{
+			Name: "MultiClusterHub",
+			GVK: schema.GroupVersionKind{
+				Group:   "operator.open-cluster-management.io",
+				Version: "v1",
+				Kind:    "MultiClusterHubList",
+			},
+		},
+	}
+
+	blockDeletionResources = []struct {
+		Name string
+		GVK  schema.GroupVersionKind
+	}{
+		{
+			Name: "ManagedCluster",
+			GVK: schema.GroupVersionKind{
+				Group:   "cluster.open-cluster-management.io",
+				Version: "v1",
+				Kind:    "ManagedClusterList",
+			},
+		},
+		{
+			Name: "BareMetalAsset",
+			GVK: schema.GroupVersionKind{
+				Group:   "inventory.open-cluster-management.io",
+				Version: "v1alpha1",
+				Kind:    "BareMetalAssetList",
+			},
+		},
+		{
+			Name: "MultiClusterObservability",
+			GVK: schema.GroupVersionKind{
+				Group:   "observability.open-cluster-management.io",
+				Version: "v1beta2",
+				Kind:    "MultiClusterObservabilityList",
+			},
+		},
+	}
 )
 
 func (r *BackplaneConfig) SetupWebhookWithManager(mgr ctrl.Manager) error {
@@ -59,7 +110,37 @@ var _ webhook.Validator = &BackplaneConfig{}
 
 // ValidateCreate implements webhook.Validator so a webhook will be registered for the type
 func (r *BackplaneConfig) ValidateCreate() error {
+	ctx := context.Background()
 	backplaneconfiglog.Info("validate create", "name", r.Name)
+
+	cfg, err := config.GetConfig()
+	if err != nil {
+		return err
+	}
+
+	c, err := discovery.NewDiscoveryClientForConfig(cfg)
+	if err != nil {
+		return err
+	}
+
+	for _, resource := range blockCreationResources {
+		list := &unstructured.UnstructuredList{}
+		list.SetGroupVersionKind(resource.GVK)
+		err := discovery.ServerSupportsVersion(c, list.GroupVersionKind().GroupVersion())
+		if err == nil {
+			if err := Client.List(ctx, list); err != nil {
+				// Server may support Group Version, but explicitly also exempt Kind
+				if strings.Contains(err.Error(), "no matches for kind") || k8serrors.IsNotFound(err) {
+					continue
+				}
+				return fmt.Errorf("unable to list %s: %s", resource.Name, err)
+			}
+			if len(list.Items) == 0 {
+				continue
+			}
+			return fmt.Errorf("cannot create %s resource. Existing %s resources must first be deleted", r.Name, resource.Name)
+		}
+	}
 
 	backplaneConfigList := &BackplaneConfigList{}
 	if err := Client.List(context.TODO(), backplaneConfigList); err != nil {
@@ -82,8 +163,33 @@ func (r *BackplaneConfig) ValidateUpdate(old runtime.Object) error {
 
 // ValidateDelete implements webhook.Validator so a webhook will be registered for the type
 func (r *BackplaneConfig) ValidateDelete() error {
-	backplaneconfiglog.Info("validate delete", "name", r.Name)
-
 	// TODO(user): fill in your validation logic upon object deletion.
+	backplaneconfiglog.Info("validate delete", "name", r.Name)
+	ctx := context.Background()
+
+	cfg, err := config.GetConfig()
+	if err != nil {
+		return err
+	}
+
+	c, err := discovery.NewDiscoveryClientForConfig(cfg)
+	if err != nil {
+		return err
+	}
+
+	for _, resource := range blockDeletionResources {
+		list := &unstructured.UnstructuredList{}
+		list.SetGroupVersionKind(resource.GVK)
+		err := discovery.ServerSupportsVersion(c, list.GroupVersionKind().GroupVersion())
+		if err == nil {
+			if err := Client.List(ctx, list); err != nil {
+				return fmt.Errorf("unable to list %s: %s", resource.Name, err)
+			}
+			if len(list.Items) == 0 {
+				continue
+			}
+			return fmt.Errorf("cannot delete %s resource. Existing %s resources must first be deleted", r.Name, resource.Name)
+		}
+	}
 	return nil
 }
