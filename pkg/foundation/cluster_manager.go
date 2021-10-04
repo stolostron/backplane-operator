e516a298c7b95fe3e574991ceede8f14fa28694d
Cameron Wall <75807506+cameronmwall@users.noreply.github.com>
2021-10-04 14:30:36 -0400

    add default tolerations to the cluster manager resource (#67)
    * add default tolerations to the cluster manager resource
    
    Signed-off-by: Cameron Wall <cwall@redhat.com>
    
    * reduce code duplication
    
    Signed-off-by: Cameron Wall <cwall@redhat.com>

--- pkg/foundation/cluster_manager.go
+++ pkg/foundation/cluster_manager.go
@@ -5,6 +5,7 @@ package foundation
 import (
 	"context"
 
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	v1alpha1 "github.com/open-cluster-management/backplane-operator/api/v1alpha1"
@@ -41,6 +42,13 @@ func PlacementImage(overrides map[string]string) string {
 func ClusterManager(m *v1alpha1.MultiClusterEngine, overrides map[string]string) *unstructured.Unstructured {
 	log := log.FromContext(context.Background())
 
+	cmTolerations := []corev1.Toleration{}
+	if m.Spec.Tolerations != nil {
+		cmTolerations = m.Spec.Tolerations
+	} else {
+		cmTolerations = utils.DefaultTolerations()
+	}
+
 	cm := &ocmapiv1.ClusterManager{
 		TypeMeta: metav1.TypeMeta{
 			APIVersion: "operator.open-cluster-management.io/v1",
@@ -55,7 +63,7 @@ func ClusterManager(m *v1alpha1.MultiClusterEngine, overrides map[string]string)
 			PlacementImagePullSpec:    PlacementImage(overrides),
 			NodePlacement: ocmapiv1.NodePlacement{
 				NodeSelector: m.Spec.NodeSelector,
-				Tolerations:  m.Spec.Tolerations,
+				Tolerations:  cmTolerations,
 			},
 		},
 	}
