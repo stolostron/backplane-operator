756e58b960359d011a38b05ef8a4de1910937b01
Zachary Kayyali <zkayyali@redhat.com>
2022-01-28 10:30:44 -0500

    Unit test webhook
    Signed-off-by: Zachary Kayyali <zkayyali@redhat.com>

--- api/v1/webhook_suite_test.go
+++ api/v1/webhook_suite_test.go
@@ -68,7 +68,7 @@ var _ = BeforeSuite(func() {
 		CRDDirectoryPaths:     []string{filepath.Join("..", "..", "config", "crd", "bases")},
 		ErrorIfCRDPathMissing: false,
 		WebhookInstallOptions: envtest.WebhookInstallOptions{
-			Paths: []string{filepath.Join("..", "..", "config", "webhook")},
+			Paths: []string{filepath.Join("..", "..", "pkg", "templates", "core")},
 		},
 	}
 
