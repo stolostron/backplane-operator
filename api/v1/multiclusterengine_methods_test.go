7f8ca2349a5825e72d9b8db05dee9af6aa42daf1
Nathan Weatherly <nweather@redhat.com>
2022-02-02 10:22:10 -0500

    adding unit tests for mce methods
    Signed-off-by: Nathan Weatherly <nweather@redhat.com>

--- api/v1/multiclusterengine_methods_test.go
+++ api/v1/multiclusterengine_methods_test.go
@@ -1,33 +1,141 @@
 // Copyright Contributors to the Open Cluster Management project
-
-/*
-Copyright 2021.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package v1
 
-// backplaneConfig := &v1.MultiClusterEngine{
-// 	TypeMeta: metav1.TypeMeta{
-// 		APIVersion: "multicluster.openshift.io/v1",
-// 		Kind:       "MultiClusterEngine",
-// 	},
-// 	ObjectMeta: metav1.ObjectMeta{
-// 		Name: BackplaneConfigName,
-// 	},
-// 	Spec: v1.MultiClusterEngineSpec{
-// 		TargetNamespace: DestinationNamespace,
-// 		ImagePullSecret: "testsecret",
-// 	},
-// }
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"testing"
+)
+
+func Test_ComponentEnabled(t *testing.T) {
+	// tracker := StatusTracker{Client: fake.NewClientBuilder().Build()}
+
+	t.Run("No components enabled", func(t *testing.T) {
+		mce := MultiClusterEngine{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "multicluster.openshift.io/v1",
+				Kind:       "MultiClusterEngine",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "test",
+			},
+			Spec: MultiClusterEngineSpec{
+				TargetNamespace: "test",
+			},
+		}
+
+		msaEnabled := mce.ComponentEnabled(ManagedServiceAccount)
+		if msaEnabled {
+			t.Fatal("Expected no component enabled, but ManagedServiceAccount enabled")
+		}
+		// FUTURE: INCLUDE ALL OTHER COMPONENT ENABLED OPTIONS HERE, ONCE THEY EXIST
+	})
+
+	t.Run("ManagedServiceAccount not enabled", func(t *testing.T) {
+		mce := MultiClusterEngine{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "multicluster.openshift.io/v1",
+				Kind:       "MultiClusterEngine",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "test",
+			},
+			Spec: MultiClusterEngineSpec{
+				TargetNamespace: "test",
+			},
+		}
+
+		msaEnabled := mce.ComponentEnabled(ManagedServiceAccount)
+		if msaEnabled {
+			t.Fatal("Expected ManagedServiceAccount to not be enabled (no ComponentConfig)")
+		}
+
+		mce = MultiClusterEngine{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "multicluster.openshift.io/v1",
+				Kind:       "MultiClusterEngine",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "test",
+			},
+			Spec: MultiClusterEngineSpec{
+				TargetNamespace: "test",
+				ComponentConfig: &ComponentConfig{},
+			},
+		}
+
+		msaEnabled = mce.ComponentEnabled(ManagedServiceAccount)
+		if msaEnabled {
+			t.Fatal("Expected ManagedServiceAccount to not be enabled (empty ComponentConfig)")
+		}
+
+		mce = MultiClusterEngine{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "multicluster.openshift.io/v1",
+				Kind:       "MultiClusterEngine",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "test",
+			},
+			Spec: MultiClusterEngineSpec{
+				TargetNamespace: "test",
+				ComponentConfig: &ComponentConfig{
+					ManagedServiceAccount: &ManagedServiceAccountConfig{},
+				},
+			},
+		}
+
+		msaEnabled = mce.ComponentEnabled(ManagedServiceAccount)
+		if msaEnabled {
+			t.Fatal("Expected ManagedServiceAccount to not be enabled (empty ManagedServiceAccountConfig)")
+		}
+
+		mce = MultiClusterEngine{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "multicluster.openshift.io/v1",
+				Kind:       "MultiClusterEngine",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "test",
+			},
+			Spec: MultiClusterEngineSpec{
+				TargetNamespace: "test",
+				ComponentConfig: &ComponentConfig{
+					ManagedServiceAccount: &ManagedServiceAccountConfig{
+						Enable: false,
+					},
+				},
+			},
+		}
+
+		msaEnabled = mce.ComponentEnabled(ManagedServiceAccount)
+		if msaEnabled {
+			t.Fatal("Expected ManagedServiceAccount to not be enabled (Enable: false)")
+		}
+	})
+
+	t.Run("ManagedServiceAccount enabled", func(t *testing.T) {
+		mce := MultiClusterEngine{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "multicluster.openshift.io/v1",
+				Kind:       "MultiClusterEngine",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "test",
+			},
+			Spec: MultiClusterEngineSpec{
+				TargetNamespace: "test",
+				ComponentConfig: &ComponentConfig{
+					ManagedServiceAccount: &ManagedServiceAccountConfig{
+						Enable: false,
+					},
+				},
+			},
+		}
+
+		msaEnabled := mce.ComponentEnabled(ManagedServiceAccount)
+		if !msaEnabled {
+			t.Fatal("Expected ManagedServiceAccount to be enabled")
+		}
+		// FUTURE: INCLUDE ALL OTHER COMPONENT ENABLED OPTIONS HERE, ONCE THEY EXIST
+	})
+}
