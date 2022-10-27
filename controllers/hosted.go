97ebb9a96cd0a38b74dcc4172484c868d8c20900
Jakob Gray <20209054+JakobGray@users.noreply.github.com>
2022-10-27 08:32:06 -0400

    WIP
    Signed-off-by: Jakob Gray <20209054+JakobGray@users.noreply.github.com>

--- controllers/hosted.go
+++ controllers/hosted.go
@@ -8,14 +8,22 @@ import (
 	"fmt"
 
 	backplanev1 "github.com/stolostron/backplane-operator/api/v1"
+	"github.com/stolostron/backplane-operator/pkg/foundation"
+	renderer "github.com/stolostron/backplane-operator/pkg/rendering"
 	"github.com/stolostron/backplane-operator/pkg/status"
+	"github.com/stolostron/backplane-operator/pkg/toggle"
 	"github.com/stolostron/backplane-operator/pkg/utils"
 
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/tools/clientcmd"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
 )
 
 var ErrBadFormat = errors.New("bad format")
@@ -80,6 +88,25 @@ func (r *MultiClusterEngineReconciler) HostedReconcile(ctx context.Context, mce 
 		return err
 	}
 
+	// Create hosted ClusterManager
+	if !mce.Enabled(backplanev1.ClusterManager) {
+		result, err := r.ensureNoClusterManager(ctx, backplaneConfig)
+		if result != (ctrl.Result{}) {
+			requeue = true
+		}
+		if err != nil {
+			errs[backplanev1.ClusterManager] = err
+		}
+	} else {
+		result, err := r.ensureClusterManager(ctx, backplaneConfig)
+		if result != (ctrl.Result{}) {
+			requeue = true
+		}
+		if err != nil {
+			errs[backplanev1.ClusterManager] = err
+		}
+	}
+
 	err = uncachedClient.Create(ctx, &corev1.Namespace{
 		ObjectMeta: metav1.ObjectMeta{Name: mce.Spec.TargetNamespace},
 	})
@@ -109,4 +136,82 @@ func parseKubeCreds(secret *corev1.Secret) ([]byte, error) {
 		return []byte{}, fmt.Errorf("%s: %w", secret.Name, ErrBadFormat)
 	}
 	return kubeconfig, nil
+}
+
+func (r *MultiClusterEngineReconciler) ensureClusterManager(ctx context.Context, backplaneConfig *backplanev1.MultiClusterEngine) (ctrl.Result, error) {
+	r.StatusManager.AddComponent(status.ClusterManagerStatus{
+		NamespacedName: types.NamespacedName{Name: "hosted-cluster-manager"},
+	})
+
+	// Apply clustermanager
+	cmTemplate := foundation.HostedClusterManager(backplaneConfig, r.Images)
+	if err := ctrl.SetControllerReference(backplaneConfig, cmTemplate, r.Scheme); err != nil {
+		return ctrl.Result{}, fmt.Errorf("Error setting controller reference on resource `%s`: %w", cmTemplate.GetName(), err)
+	}
+	force := true
+	err := r.Client.Patch(ctx, cmTemplate, client.Apply, &client.PatchOptions{Force: &force, FieldManager: "backplane-operator"})
+	if err != nil {
+		return ctrl.Result{}, errors.Wrapf(err, "error applying object Name: %s Kind: %s", cmTemplate.GetName(), cmTemplate.GetKind())
+	}
+
+	return ctrl.Result{}, nil
+}
+
+func (r *MultiClusterEngineReconciler) ensureNoClusterManager(ctx context.Context, backplaneConfig *backplanev1.MultiClusterEngine) (ctrl.Result, error) {
+	log := log.FromContext(ctx)
+	namespacedName := types.NamespacedName{Name: "cluster-manager", Namespace: backplaneConfig.Spec.TargetNamespace}
+
+	// Renders all templates from charts
+	templates, errs := renderer.RenderChart(toggle.ClusterManagerChartDir, backplaneConfig, r.Images)
+	if len(errs) > 0 {
+		for _, err := range errs {
+			log.Info(err.Error())
+		}
+		return ctrl.Result{RequeueAfter: requeuePeriod}, nil
+	}
+
+	r.StatusManager.RemoveComponent(toggle.EnabledStatus(namespacedName))
+	r.StatusManager.AddComponent(toggle.DisabledStatus(namespacedName, []*unstructured.Unstructured{}))
+	r.StatusManager.RemoveComponent(status.ClusterManagerStatus{
+		NamespacedName: types.NamespacedName{Name: "cluster-manager"},
+	})
+
+	// Delete clustermanager
+	clusterManager := &unstructured.Unstructured{}
+	clusterManager.SetGroupVersionKind(
+		schema.GroupVersionKind{
+			Group:   "operator.open-cluster-management.io",
+			Version: "v1",
+			Kind:    "ClusterManager",
+		},
+	)
+	err := r.Client.Get(ctx, types.NamespacedName{Name: "cluster-manager"}, clusterManager)
+	if err == nil { // If resource exists, delete
+		err := r.Client.Delete(ctx, clusterManager)
+		if err != nil {
+			return ctrl.Result{RequeueAfter: requeuePeriod}, err
+		}
+	} else if err != nil && !apierrors.IsNotFound(err) { // Return error, if error is not not found error
+		return ctrl.Result{RequeueAfter: requeuePeriod}, err
+	}
+
+	// Verify clustermanager namespace deleted
+	ocmHubNamespace := &corev1.Namespace{}
+	err = r.Client.Get(ctx, types.NamespacedName{Name: "open-cluster-management-hub"}, ocmHubNamespace)
+	if err == nil {
+		return ctrl.Result{RequeueAfter: requeuePeriod}, fmt.Errorf("waiting for 'open-cluster-management-hub' namespace to be terminated before proceeding with clustermanager cleanup")
+	}
+	if err != nil && !apierrors.IsNotFound(err) { // Return error, if error is not not found error
+		return ctrl.Result{RequeueAfter: requeuePeriod}, err
+	}
+
+	// Deletes all templates
+	for _, template := range templates {
+		result, err := r.deleteTemplate(ctx, backplaneConfig, template)
+		if err != nil {
+			log.Error(err, fmt.Sprintf("Failed to delete template: %s", template.GetName()))
+			return result, err
+		}
+	}
+	return ctrl.Result{}, nil
 }
---
+++ pkg/foundation/hosted_cluster_manager.go
@@ -0,0 +1,68 @@
+// Copyright Contributors to the Open Cluster Management project
+
+package foundation
+
+import (
+	"context"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	v1 "github.com/stolostron/backplane-operator/api/v1"
+	"github.com/stolostron/backplane-operator/pkg/utils"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	ocmapiv1 "open-cluster-management.io/api/operator/v1"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+// HostedClusterManager returns the ClusterManager in hosted mode
+func HostedClusterManager(m *v1.MultiClusterEngine, overrides map[string]string) *unstructured.Unstructured {
+	log := log.FromContext(context.Background())
+
+	cmTolerations := []corev1.Toleration{}
+	if m.Spec.Tolerations != nil {
+		cmTolerations = m.Spec.Tolerations
+	} else {
+		cmTolerations = utils.DefaultTolerations()
+	}
+
+	cm := &ocmapiv1.ClusterManager{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "operator.open-cluster-management.io/v1",
+			Kind:       "ClusterManager",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "hosted-cluster-manager",
+		},
+		Spec: ocmapiv1.ClusterManagerSpec{
+			RegistrationImagePullSpec: RegistrationImage(overrides),
+			WorkImagePullSpec:         WorkImage(overrides),
+			PlacementImagePullSpec:    PlacementImage(overrides),
+			NodePlacement: ocmapiv1.NodePlacement{
+				NodeSelector: m.Spec.NodeSelector,
+				Tolerations:  cmTolerations,
+			},
+			DeployOption: ocmapiv1.ClusterManagerDeployOption{
+				Mode: ocmapiv1.InstallModeHosted,
+				Hosted: &ocmapiv1.HostedClusterManagerConfiguration{
+					RegistrationWebhookConfiguration: ocmapiv1.WebhookConfiguration{
+						Address: "192.168.218.1",
+						Port:    443,
+					},
+					WorkWebhookConfiguration: ocmapiv1.WebhookConfiguration{
+						Address: "192.168.218.2",
+						Port:    443,
+					},
+				},
+			},
+		},
+	}
+
+	utils.AddBackplaneConfigLabels(cm, m.GetName())
+	unstructured, err := utils.CoreToUnstructured(cm)
+	if err != nil {
+		log.Error(err, err.Error())
+	}
+
+	return unstructured
+}
