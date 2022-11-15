5070db08b27a343b5c3fa0a8093d15f9ae353e95
Jakob Gray <20209054+JakobGray@users.noreply.github.com>
2022-11-15 14:01:51 -0500

    Configure TLS minimum version to 1.2 (#320)
    Signed-off-by: Jakob Gray <20209054+JakobGray@users.noreply.github.com>
    
    Signed-off-by: Jakob Gray <20209054+JakobGray@users.noreply.github.com>

--- main.go
+++ main.go
@@ -55,6 +55,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
 
 	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
 	backplanev1 "github.com/stolostron/backplane-operator/api/v1"
@@ -127,6 +128,7 @@ func main() {
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
 		LeaderElectionID:       "797f9276.open-cluster-management.io",
+		WebhookServer:          &webhook.Server{TLSMinVersion: "1.2"},
 		// LeaderElectionNamespace: "backplane-operator-system", // Ensure this is commented out. Uncomment only for running operator locally.
 	})
 	if err != nil {
