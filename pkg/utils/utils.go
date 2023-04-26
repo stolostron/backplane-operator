c8bff98e7bc9cbe0f936dba52d80a8595b88bf7f
Roke Jung <roke@redhat.com>
2023-04-26 18:50:34 -0400

    fix test
    Signed-off-by: Roke Jung <roke@redhat.com>

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -171,7 +171,7 @@ func GetTestImages() []string {
 		"assisted_service", "assisted_image_service", "postgresql_12", "assisted_installer_agent", "assisted_installer_controller",
 		"assisted_installer", "console_mce", "hypershift_addon_operator", "hypershift_operator",
 		"apiserver_network_proxy", "aws_encryption_provider", "cluster_api", "cluster_api_provider_agent", "cluster_api_provider_aws",
-		"cluster_api_provider_azure", "cluster_api_provider_kubevirt", "cluster_proxy_addon", "cluster_proxy", "cluster_image_set_controller"}
+		"cluster_api_provider_azure", "cluster_api_provider_kubevirt", "kube_rbac_proxy_mce", "cluster_proxy_addon", "cluster_proxy", "cluster_image_set_controller"}
 }
 
 func IsUnitTest() bool {
