42cba7936e3432ced0f090b20c670d3787f71300
zhujian <jiazhu@redhat.com>
2023-09-21 05:40:41 +0000

    Update chart for managed service account addon
    Signed-off-by: zhujian <jiazhu@redhat.com>

--- pkg/rendering/renderer_test.go
+++ pkg/rendering/renderer_test.go
@@ -171,7 +171,9 @@ func TestRender(t *testing.T) {
 			containsHTTPS = false
 			containsNO = false
 		}
-		if template.GetKind() == "AddOnDeploymentConfig" {
+		if template.GetKind() == "AddOnDeploymentConfig" &&
+			// managed service account addon deployment config does not need a node placement config
+			template.GetName() != "managed-serviceaccount-addon-deploy-config" {
 			addonDep := &addonv1alpha1.AddOnDeploymentConfig{}
 			err := runtime.DefaultUnstructuredConverter.FromUnstructured(template.Object, addonDep)
 			if err != nil {
