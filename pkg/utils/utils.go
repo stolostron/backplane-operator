383f05863e3ebbeed96f7fdc191eb8f828d6548a
dislbenn <dbennett@redhat.com>
2024-08-12 12:24:59 -0400

    updated IBIO component to be disabled by default
    Signed-off-by: dislbenn <dbennett@redhat.com>

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -45,12 +45,13 @@ var onComponents = []string{
 	backplanev1.LocalCluster,
 	backplanev1.HypershiftLocalHosting,
 	backplanev1.HyperShift,
-	backplanev1.ImageBasedInstallOperator,
 	backplanev1.ManagedServiceAccount,
 	// backplanev1.ConsoleMCE, // determined by OCP version
 }
 
-var offComponents = []string{}
+var offComponents = []string{
+	backplanev1.ImageBasedInstallOperator,
+}
 
 // SetDefaultComponents returns true if changes are made
 func SetDefaultComponents(m *backplanev1.MultiClusterEngine) bool {
