eb41b7c8f9b0c089799bdde3e7ef0a47568d21ad
Nathaniel Graham <ngraham@redhat.com>
2024-08-21 15:07:52 -0600

    re-disabled consoleMCE
    Signed-off-by: Nathaniel Graham <ngraham@redhat.com>

--- controllers/backplaneconfig_controller_test.go
+++ controllers/backplaneconfig_controller_test.go
@@ -571,7 +571,7 @@ var _ = Describe("BackplaneConfig controller", func() {
 								},
 								{
 									Name:    backplanev1.ConsoleMCE,
-									Enabled: true,
+									Enabled: false,
 								},
 								{
 									Name:    backplanev1.Discovery,
