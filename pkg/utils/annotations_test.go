759409798551b3d49eeb3caf7d59dcba5c05eded
Cameron Wall <cwall@redhat.com>
2025-01-23 11:52:27 -0500

    ut
    Signed-off-by: Cameron Wall <cwall@redhat.com>

--- pkg/utils/annotations_test.go
+++ pkg/utils/annotations_test.go
@@ -87,6 +87,23 @@ func Test_AnnotationMatch(t *testing.T) {
 	}
 }
 
+func Test_getAnnotationFlightControl(t *testing.T) {
+	instance := &backplanev1.MultiClusterEngine{
+		ObjectMeta: metav1.ObjectMeta{Annotations: map[string]string{AnnotationFlightEnabled: "true"}},
+	}
+	result := GetFightEnabled(instance)
+	if result != "true" {
+		t.Errorf("Was expecting true for the flight control annotation. Did not receive")
+	}
+	instance = &backplanev1.MultiClusterEngine{
+		ObjectMeta: metav1.ObjectMeta{},
+	}
+	result = GetFightEnabled(instance)
+	if result != "false" {
+		t.Errorf("Was expecting false for the flight control annotation. Did not receive")
+	}
+}
+
 func Test_getAnnotation(t *testing.T) {
 	type args struct {
 		instance *backplanev1.MultiClusterEngine
