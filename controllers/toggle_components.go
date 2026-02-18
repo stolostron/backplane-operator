30950b39432618b0658b24003f928f508f41cab6
Cameron Wall <75807506+cameronmwall@users.noreply.github.com>
2026-02-18 13:57:21 -0500

    capm3 chart refresh and deployment rename (#2800)
    Signed-off-by: Cameron Wall <cwall@redhat.com>
    Co-authored-by: Disaiah Bennett <dbennett@redhat.com>

--- controllers/toggle_components.go
+++ controllers/toggle_components.go
@@ -625,7 +625,7 @@ func (r *MultiClusterEngineReconciler) ensureNoClusterAPIProviderAzure(ctx conte
 func (r *MultiClusterEngineReconciler) ensureClusterAPIProviderMetal(ctx context.Context, mce *backplanev1.MultiClusterEngine) (
 	ctrl.Result, error) {
 
-	namespacedName := types.NamespacedName{Name: "capm3-controller-manager", Namespace: mce.Spec.TargetNamespace}
+	namespacedName := types.NamespacedName{Name: "mce-capm3-controller-manager", Namespace: mce.Spec.TargetNamespace}
 	r.StatusManager.RemoveComponent(toggle.DisabledStatus(namespacedName, []*unstructured.Unstructured{}))
 	r.StatusManager.AddComponent(toggle.EnabledStatus(namespacedName))
 
@@ -664,7 +664,7 @@ func (r *MultiClusterEngineReconciler) ensureClusterAPIProviderMetal(ctx context
 
 func (r *MultiClusterEngineReconciler) ensureNoClusterAPIProviderMetal(ctx context.Context,
 	mce *backplanev1.MultiClusterEngine) (ctrl.Result, error) {
-	namespacedName := types.NamespacedName{Name: "capm3-controller-manager", Namespace: mce.Spec.TargetNamespace}
+	namespacedName := types.NamespacedName{Name: "mce-capm3-controller-manager", Namespace: mce.Spec.TargetNamespace}
 
 	// Ensure that the InternalHubComponent CR instance is deleted for component in MCE.
 	if result, err := r.ensureNoInternalEngineComponent(ctx, mce,
